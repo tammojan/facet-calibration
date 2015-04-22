c345a05803a850fe06f7875ca4819309420c302b
Reinout van Weeren <rvanweeren@cfa.harvard.edu>
2015-04-22 05:55:07 -0400

    initial

---
+++ fits2image.py
@@ -0,0 +1,21 @@
+import os
+import sys
+import numpy
+
+el=len(sys.argv)
+print sys.argv[:]
+
+inputim     = sys.argv[el-2]
+imageout  = sys.argv[el-1]
+
+
+print 'inputim', inputim
+print 'imageout', imageout
+
+os.system('rm -rf ' + imageout)
+
+importfits(fitsimage=inputim,imagename=imageout,whichrep=0,\
+           whichhdu=-1,zeroblanks=True,overwrite=True,defaultaxes=False,\
+	   defaultaxesvalues=[],beam=[])
+
+
---
+++ fitsandregion2image.py
@@ -0,0 +1,48 @@
+import os
+import sys
+import numpy
+
+el=len(sys.argv)
+#print sys.argv[:]
+
+inputim    = sys.argv[el-3]
+imageout  = sys.argv[el-2]
+region    = sys.argv[el-1]
+
+print 'inputim', inputim
+print 'imageout', imageout
+print 'region', region
+
+importfits(fitsimage=inputim,imagename=imageout,whichrep=0,\
+           whichhdu=-1,zeroblanks=True,overwrite=True,defaultaxes=False,\
+	   defaultaxesvalues=[],beam=[])
+
+
+if region != 'None':
+ print 'We have a region file', region
+ ia.open(imageout)
+ myRGN = rg.fromtextfile(filename=region, shape=ia.shape(), csys=ia.coordsys().torecord())
+ im.regiontoimagemask(mask=imageout, region=myRGN)
+ ia.close()
+
+# avoid cleaning the edges of the image
+edge = 25
+tb.open(imageout, nomodify=False)
+data = numpy.copy(tb.getcol('map'))
+
+print numpy.shape(data)
+sh = numpy.shape(data)
+print sh
+
+
+# mask edges
+data[0:sh[0],0:edge,0,0,0] = 0
+data[0:edge,0:sh[1],0,0,0] = 0
+data[0:sh[0],sh[1]-edge:sh[1],0,0,0] = 0
+data[sh[0]-edge:sh[0],0:sh[1],0,0,0] = 0
+
+
+
+tb.putcol('map', data)
+tb.flush()
+tb.close()
