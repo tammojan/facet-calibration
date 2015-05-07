ff6663a150a3bfef89a832970327fa4f24c87d96
Martin Hardcastle <mjh@extragalactic.info>
2015-05-07 11:50:07 +0100

    initial fixes to use parset

---
+++ suball-parset.py
@@ -0,0 +1,6 @@
+# Example subtractallcc_wsclean.py parset
+
+SCRIPTPATH='/home/mjh/git/facet-calibration'
+wsclean         =  '/home/mjh/wsclean-1.7/build/wsclean'
+mslist=['NW_SB240-249.2ch10s.ms']
+casaregion=''
