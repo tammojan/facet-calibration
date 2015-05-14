ed2dc967560a23b800d2c43ce19f9bb830300a33
José Sabater Montes <jsm@iaa.es>
2015-05-14 17:56:14 +0100

    Correct problem with data types

--- doDDE_v21_a2256.py
+++ doDDE_v21_a2256.py
@@ -1622,17 +1622,17 @@ if __name__ == "__main__":
 
     freq_tab     = pt.table(mslist[0]  + '/SPECTRAL_WINDOW')
     numchanperms = freq_tab.getcol('NUM_CHAN')[0]
-    logging.info('Number of channels per ms is ', numchanperms)
+    logging.info('Number of channels per ms is {:d}'.format(numchan_all))
     freq_tab.close()
 
     freq_tab     = pt.table(allbandspath +'allbands.concat.ms/SPECTRAL_WINDOW')
     numchan_all = freq_tab.getcol('NUM_CHAN')[0]
-    logging.info('Number of channels allbands.concat.ms is '+numchan_all)
+    logging.info('Number of channels allbands.concat.ms is {:d}'.format(numchan_all))
     freq_tab.close()
     
     if (numchanperms*len(mslistorig)) != numchan_all:
         logging.error('Used a total numbers of ' + str(len(mslistorig)) + ' blocks with ' + str(numchanperms) + ' channels per block')
-        logging.error('Number of channels allbands.concat.ms is ' + numchan_all)
+        logging.error('Number of channels allbands.concat.ms is {:d}'.format(numchan_all))
         raise Exception('#channels in allbands.concat.ms does not match with what is expected from mslistorig (from parameter BANDS)')
 
     #if len(mslist) == 1:
