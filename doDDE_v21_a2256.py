8f95aad7465de27d61cdd846272ce1f5ee08dc8b
Reinout van Weeren <rvanweeren@cfa.harvard.edu>
2015-06-05 23:44:31 -0400

    fix NDPPP check, only do that in case of preSC

--- doDDE_v21_a2256.py
+++ doDDE_v21_a2256.py
@@ -1822,7 +1822,9 @@ if __name__ == "__main__":
 
         if delete_SC_ms: # delete selfcal MS, default is False
             os.system('rm -rf ' + '*.' + source + '.ms')
-        ndallbands.wait() # wait for NDPPP run for allbands.concat.ms
+        
+        if (StartAtStep in ['preSC']):
+            ndallbands.wait() # wait for NDPPP run for allbands.concat.ms
 
         parmdb_master_out="instrument_master_" + source
         if (outliersource[source_id] == 'False') or doOUTLIER_withGaussfix:
